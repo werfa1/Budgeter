23e3ad451e9ff6866eabfe1c13da979ba2f00c33
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2025-12-26 14:13:26 -0800

    Initial Commit

---
+++ Budgeter/BudgeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeterApp.swift
+//  Budgeter
+//
+//  Created by Pasha Otverchenko on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgeterTests/BudgeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeterTests.swift
+//  BudgeterTests
+//
+//  Created by Pasha Otverchenko on 26.12.2025.
+//
+
+import Testing
+@testable import Budgeter
+
+struct BudgeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
